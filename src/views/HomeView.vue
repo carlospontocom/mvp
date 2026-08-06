64ccab4ce590db4ccd90215603abc8df92847e3d
Carlos Nascimento <frontendcnn@gmail.com>
2026-08-05 23:55:59 -0300

    feat:erro sanado - temas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,6 @@
 <template>
  <section id="home" class="home-section">
-    <!--<div class="container">
-    <h1>Agende seus Serviços Bancários com Facilidade</h1>
-    <p>
-      Diga adeus às filas e à espera interminável. Nosso sistema de 
-      <strong>agendamento bancário online</strong> permite que você escolha 
-      o melhor horário para ser atendido, garantindo praticidade e segurança 
-      em cada interação.
-    </p>
-    <p>
-      Seja para abertura de contas, consultoria financeira, renegociação de 
-      dívidas ou atendimento personalizado, oferecemos uma solução moderna 
-      que coloca você no controle da sua rotina bancária.
-    </p>
-    <p>
-      Com notificações automáticas e gestão inteligente de horários, 
-      seu tempo é valorizado e sua experiência bancária se torna muito 
-      mais eficiente.
-    </p>
-    <router-link to="/contatos" class="btn-primary">
-  Agendar Atendimento
-</router-link> -->
+    
 
 <div>
   <h1>FAQ-Atendimento</h1>
@@ -34,8 +14,7 @@
   <LoginView/> 
 </div>
 
-  </div>
-</section>
+ </section>
 
 </template>
 
